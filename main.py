797e736f3a52da3bdf1d00a846b8228e4d8ee122
bossmuller <bossvill.witaba@strathmore.edu>
2022-09-23 07:31:26 +0300

    Learning python is ongoing

---
+++ main.py
@@ -0,0 +1,99 @@
+
+#i = 5
+
+#pi=3.14
+
+#Area= i *3.14
+
+#print("The area of a circle is: ",Area)
+
+#name = input("What is your name: ")
+#print (name)
+
+# n1= int(input("Enter the first number: "))
+# n2= int(input("Enter the Second number: "))
+
+# Sum = n1 + n2
+
+# print("The sum is: ", Sum)
+
+# Operators in Python
+# Arithmetics operators
+# addition
+# x=5
+# result =x + 10
+# print(result)
+
+# Subtraction
+# x=5
+# result =x - 10
+# print(result)
+# Quotient
+# from math import remainder
+
+
+# x=5
+# quotient =x //2
+# remainder = x % 2
+# print("The quotient is : " , quotient ,"and" ,"the remaider is: " , remainder)
+# fee = 4535
+
+# discount = 10
+
+# discount_amount = (discount /100) * fee
+
+# discounted_fee = fee - discount_amount
+# print("The fee after discount is : " , discounted_fee)
+
+# from tokenize import Double
+# from turtle import distance
+
+
+# distance_km = 850
+
+# distance_mile = float(distance_km * 0.621371)
+
+# print("The distance in miles is: " , distance_mile)
+
+# Comparison operators
+# < Less than
+# > Greater than
+# == Equal
+# != Not equal
+# >= Greater than or equal to
+# <= Less than or equal to
+# 
+# Boolean
+# True or False
+# n=5
+# print(n<10)
+
+
+# Logical Operators
+# and True if both operands are True
+# or True if either of the operands is True
+# not True if the operand is False
+
+# age= 22
+# gpa= 3.8
+
+# result = age >= 18 and gpa  > 3.6
+# print(result)
+
+# Decision Making
+Marks = int(input("Enter your marks: "))
+if Marks >= 40:
+    print("You passed the exams , Congratulation")
+elif Marks > 30:
+    print("You failed the unit, take a reseat")
+
+else:
+    print("You failed the unit, take a repeat")
+    
+
+
+
+
+
+
+
